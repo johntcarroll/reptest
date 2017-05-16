10d3f4c185fa9ccf96de6e89e88be5a86e991871
John <john@John-Carroll.home>
2017-05-16 16:01:41 -0400

    testing new read loaction

--- reptest.php
+++ reptest.php
@@ -1,9 +1,9 @@
 <?php
 include "controller/config.php";
-$new = new Car();
-$new->make = "chevy";
-$new->model = "impala";
-$new->save();
+//$new = new Car();
+//$new->make = "chevy";
+//$new->model = "impala";
+//$new->save();
 $query = Car::all();
 print_r($query);
 
