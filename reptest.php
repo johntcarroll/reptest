9b6a8dca1caef9046f265f5aa305b6d91b0c51d8
John <john@John-Carroll.home>
2017-05-16 15:48:57 -0400

    testing east server

--- reptest.php
+++ reptest.php
@@ -1,9 +1,9 @@
 <?php
 include "controller/config.php";
-$new = new Car();
-$new->make = "dodge";
-$new->model = "ram";
-$new->save();
+//$new = new Car();
+//$new->make = "dodge";
+//$new->model = "ram";
+//$new->save();
 $query = Car::all();
 print_r($query);
 
