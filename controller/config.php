5a82276e7ea704cb99ad8ae842d81a34b06146c4
John <john@John-Carroll.home>
2017-05-16 11:36:00 -0400

    bug fix

--- controller/config.php
+++ controller/config.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/../php-activerecord/ActiveRecord.php";
 require_once "/home/ec2-user/location_config.php";
 
-ActiveRecord\Config::initialize(function($cfg) use ($location_connections)
+ActiveRecord\Config::initialize(function($cfg) use ($local_connections)
  {
     $modelDir = substr(__FILE__, 0, -21) . "model";
      $cfg->set_model_directory($modelDir);
